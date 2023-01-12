7972ba25e673c792c53019cd6c15baa2d611de26
Sasha Grygoriev <truedevelop@yahoo.com>
2023-01-12 09:14:12 +0200

    first part alert component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import ReactDOM from "react-dom/client";
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Navbar from "./components/navbar/Navbar";
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Alert from "./components/alert/Alert";
+import { AlertState } from "./context/alert/AlertState";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <div className="container pt-4">
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="about" element={<About/>}/>
-          <Route path="profile/:name" element={<Profile/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar/>
+        <div className="container pt-4">
+          <Alert alert ={{text: 'Text Alert!'}}/>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="about" element={<About/>}/>
+            <Route path="profile/:name" element={<Profile/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AlertState>
 
   );
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -9,7 +9,7 @@ const Card = () => {
       <img src={""} alt={""} className='card-img-top' />
       <div className="card-body">
         <h5 className="card-title">React JS</h5>
-        <Link to={'profile/' + 'react'} className='btn btn-primary'>OPEN</Link>
+        <Link to={'profile/'} className='btn btn-primary'>OPEN</Link>
       </div>
     </div>
   );
